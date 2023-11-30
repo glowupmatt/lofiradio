78b360a5a0a696565ba04fb30dc03ac6fbb55ce9
glowupmatt <glowupmatt@gmail.com>
2023-11-29 17:56:07 -0800

    added new changes to controls

--- src/components/playerControls/SongControls.tsx
+++ src/components/playerControls/SongControls.tsx
@@ -7,6 +7,7 @@ import controlStyles from './SongControls.module.css'
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import PauseIcon from '@mui/icons-material/Pause';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 
 type Props = {
@@ -26,32 +27,46 @@ type Props = {
 }
 
 const SongControls = (props: Props) => {
-  const { selectedSong } = useContext(DataContext);
+  const { selectedSong, isPlaying, setIsPlaying } = useContext(DataContext);
   const { findAlbumThanHoldsSelectedSong } = props;
   const {image, title, artist} = findAlbumThanHoldsSelectedSong[0]
   const {name, credits, audio, id} = selectedSong
   return (
       <div className={controlStyles.infoContainer}>
-        <Image 
-        src={image} 
-        alt={name}
-        width={1080} 
-        height={1920}
-        className={controlStyles.coverArt}/> 
-        <div className={controlStyles.infoText}>
-          <h3 className={controlStyles.songName}>{name}</h3>
-          <p className={controlStyles.artist}>{artist}</p>
+        <div className={controlStyles.songDetails}>
+          <Image 
+          src={image} 
+          alt={name}
+          width={1080} 
+          height={1920}
+          className={controlStyles.coverArt}/> 
+          <div className={controlStyles.infoText}>
+            <h3 className={controlStyles.songName}>{name}</h3>
+            <p className={controlStyles.artist}>{artist}</p>
+          </div>
         </div>
-        <div className={controlStyles.controlsContainer}>
-          <SkipPreviousIcon
-          className={controlStyles.control}
-          sx={{color: "white", width:'1.5rem', height:'1.5rem'}}/>
-          <PlayCircleIcon
-          className={controlStyles.control} 
-          sx={{color: "white" , width:'1.5rem', height:'1.5rem'}}/>
-          <SkipNextIcon
-          className={controlStyles.control} 
-          sx={{color: "white" , width:'1.5rem', height:'1.5rem'}}/>
+        <div className={controlStyles.mainControlsContainer}>
+          <div className={controlStyles.controlsContainer}>
+            <SkipPreviousIcon
+            className={controlStyles.control}
+            sx={{color: "white", width:'1.5rem', height:'1.5rem'}}/>
+             {isPlaying ?
+           (<PlayCircleIcon
+           onClick={() => setIsPlaying((prev) => !prev)}
+            className={controlStyles.control} 
+            sx={{color: "white" , width:'1.5rem', height:'1.5rem'}}/>)
+            : <PauseIcon className={controlStyles.control} 
+            sx={{color: "white" , width:'1.5rem', height:'1.5rem'}}/>}
+            <SkipNextIcon
+            onClick={() => setIsPlaying((prev) => !prev)}
+            className={controlStyles.control} 
+            sx={{color: "white" , width:'1.5rem', height:'1.5rem'}}/>
+          </div>
+          <input type="range" className={controlStyles.songSlider}/>
+        </div>
+        <div className={controlStyles.audioControls}>
+          <VolumeUpIcon  sx={{color: "white" , width:'1.5rem', height:'1.5rem'}}/>
+          <input type="range" className={controlStyles.audioSlider}/>
         </div>
       </div>
   )
